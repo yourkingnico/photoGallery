e14b2481d3276fc65d1923e59c3d8891cd10fb9f
yourkingnico <nicos11@comcast.net>
2019-06-19 12:47:51 -0400

    update assets

--- photo-gallery/src/config.js
+++ photo-gallery/src/config.js
@@ -10,6 +10,10 @@ const bwca17 = [
       description: 'Going for a swim'
     },
     {
+      original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01866.JPG',
+      thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01866.JPG',
+    },
+    {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01867.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01867.JPG',
       description: 'Going for a swim'
@@ -17,7 +21,6 @@ const bwca17 = [
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01870.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01870.JPG',
-      description: 'Going for a swim'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01872.JPG',
@@ -27,17 +30,15 @@ const bwca17 = [
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01876.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01876.JPG',
-      description: 'Going for a swim'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01880.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01880.JPG',
-      description: 'Going for a swim'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01891.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01891.JPG',
-      description: 'Going for a swim'
+      description: 'Quick detour over a beaver dam'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01898.JPG',
@@ -47,32 +48,30 @@ const bwca17 = [
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01905.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01905.JPG',
-      description: 'Lunch on Mesaba Lake, MN'
+      description: 'Dinner on Little Saganaga'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01907.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01907.JPG',
-      description: 'Lunch on Mesaba Lake, MN'
+      description: "The first night's campsite"
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01912.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01912.JPG',
-      description: 'Lunch on Mesaba Lake, MN'
+      description: '"Yea, a bear can probably reach that.."'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01914.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01914.JPG',
-      description: 'Lunch on Mesaba Lake, MN'
+      description: 'Little Saganaga Lake, MN'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01922.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01922.JPG',
-      description: 'How to hike with a canoe 101'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01929.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01929.JPG',
-      description: 'How to hike with a canoe 101'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01933.JPG',
@@ -82,124 +81,172 @@ const bwca17 = [
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01936.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01936.JPG',
-      description: 'How to hike with a canoe 101'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01938.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01938.JPG',
-      description: 'How to hike with a canoe 101'
+      description: 'A pair of loons'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01941.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01941.JPG',
-      description: 'How to hike with a canoe 101'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01954.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01954.JPG',
-      description: 'How to hike with a canoe 101'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01962.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01962.JPG',
-      description: 'How to hike with a canoe 101'
+      description: 'Hiking with a canoe can be a great workout'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01967.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01967.JPG',
-      description: 'How to hike with a canoe 101'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01984.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01984.JPG',
-      description: 'How to hike with a canoe 101'
+      description: 'The remains of a moose'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01986.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC01986.JPG',
-      description: 'How to hike with a canoe 101'
+      description: 'Moose skull'
     },
     {
       original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC02002.JPG',
       thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca17/DSC02002.JPG',
-      description: 'How to hike with a canoe 101'
+      description: 'Our island campsite'
     }
 ];
 
 const bwca14 = [
   {
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1706.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1706.JPG',
-    description: 'Duluth, MN',
-    originalClass: 'mobile'
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1706.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1706.png',
+    description: 'Duluth, MN'
   },
   {
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1718.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1718.JPG',
-    description: 'Walleye for dinner',
-    originalClass: 'mobile'
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1718.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1718.png',
+    description: 'Walleye for dinner'
   },
   {
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1720.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1720.JPG',
-    description: 'Lake Polly, MN',
-    originalClass: 'mobile'
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1720.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1720.png',
+    description: 'Lake Polly, MN'
   },
   {
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1733.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1733.JPG',
-    originalClass: 'mobile'
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1725.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1725.png'
   },
   {
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1742 (4).JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1742 (4).JPG'
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1733.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1733.png'
   },
   {
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1746.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1746.JPG',
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1739.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1739.png'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1742 (4).png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1742 (4).png'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1743.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1743.png',
+    description: 'Mesaba, MN'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1746.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1746.png',
     description: 'Mesaba Lake, MN'
   },
   {
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1747.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1747.JPG',
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1747.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1747.png',
     description: 'Mesaba Lake, MN',
-    originalClass: 'mobile'
   },
   {
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1754.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1754.JPG',
-    description: 'Northern Pike',
-    originalClass: 'mobile'
-  },{
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1760.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1760.JPG',
-    description: 'Mesaba Lake, MN',
-    originalClass: 'mobile'
-  },{
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1764.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1764.JPG',
-    description: 'Mesaba Lake, MN',
-    originalClass: 'mobile'
-  },{
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1765.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1765.JPG',
-    description: 'Mesaba Lake, MN',
-    originalClass: 'mobile'
-  },{
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1766.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1766.JPG',
-    description: 'Mesaba Lake, MN',
-    originalClass: 'mobile'
-  },{
-    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1768.JPG',
-    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1768.JPG',
-    description: 'Sawbill Lake, MN',
-    originalClass: 'mobile'
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1754.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1754.png',
+    description: 'Northern Pike'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1760.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1760.png',
+    description: 'Mesaba Lake, MN'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1764.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1764.png',
+    description: 'Mesaba Lake, MN'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1765.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1765.png',
+    description: 'Mesaba Lake, MN'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1766.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1766.png',
+    description: 'Northern Pike'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1768.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca14/IMG_1768.png',
+    description: 'Northern Pike'
   },
   
 ];
 
 const bwca15 = [
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2398.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2398.png',
+    description: ''
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2419.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2419.png',
+    description: 'We started the trip to a rainy first day'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2422.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2422.png',
+    description: ''
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2423 (2).png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2423 (2).png',
+    description: 'Dan doing some fishing'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2427.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2427.png',
+    description: ''
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2436.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2436.png',
+    description: ''
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2438.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2438.png',
+    description: 'Sam with a decent pike'
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2443.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2443.png',
+    description: "Our second day's campsite"
+  },
+  {
+    original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2449.png',
+    thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/IMG_2449.png',
+    description: 'Portaging from lake to lake can be hard work'
+  },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06833.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06833.JPG',
@@ -213,32 +260,26 @@ const bwca15 = [
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06844.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06844.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06849.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06849.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06851.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06851.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06852.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06852.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06857.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06857.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06859.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06859.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06874.JPG',
@@ -253,7 +294,7 @@ const bwca15 = [
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06880.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06880.JPG',
-    description: ''
+    description: 'Getting ready for some fishing'
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06896.JPG',
@@ -268,22 +309,19 @@ const bwca15 = [
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06906.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06906.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06912.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06912.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06915.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06915.JPG',
-    description: ''
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06916.JPG',
     thumbnail: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06916.JPG',
-    description: ''
+    description: 'Fires at night did little to prevent the mosquitos...'
   },
   {
     original: 'https://s3.amazonaws.com/nicktorres.net/nicks_site/images/bwca15/DSC06920 (2).JPG',
